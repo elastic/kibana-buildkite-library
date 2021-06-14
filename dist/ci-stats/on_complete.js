de6c1a9b6cce4c3f17a4740955a471db5d1c89d0
Brian Seeders <brian.seeders@elastic.co>
2021-06-14 12:30:45 -0400

    Run build

--- dist/ci-stats/on_complete.js
+++ dist/ci-stats/on_complete.js
@@ -9,6 +9,7 @@ async function onComplete() {
     if (process.env.CI_STATS_BUILD_ID) {
         const result = buildkite.getBuildStatus(await buildkite.getCurrentBuild());
         const status = result.success ? 'SUCCESS' : 'FAILURE';
+        console.log('Job Status:', result);
         await ciStats.completeBuild(status);
     }
 }
