4340db9e1717fedee889753167690d303d957c69
Brian Seeders <brian.seeders@elastic.co>
2021-09-15 17:50:22 -0400

    Add CI Stats report to onComplete

--- dist/ci-stats/on_complete.js
+++ dist/ci-stats/on_complete.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.onComplete = void 0;
+const child_process_1 = require("child_process");
 const buildkite_1 = require("../buildkite");
 const client_1 = require("./client");
 const buildkite = new buildkite_1.BuildkiteClient();
@@ -11,6 +12,15 @@ async function onComplete() {
         const status = result.success ? 'SUCCESS' : 'FAILURE';
         console.log('Job Status:', result);
         await ciStats.completeBuild(status);
+        if (process.env.GITHUB_PR_NUMBER) {
+            const report = await ciStats.getPrReport(process.env.CI_STATS_BUILD_ID);
+            if (report === null || report === void 0 ? void 0 : report.md) {
+                process.env.CI_STATS_REPORT = report.md;
+                child_process_1.execSync('buildkite-agent meta-data set pr_comment:ci_stats_report:body "$CI_STATS_REPORT"', {
+                    stdio: 'inherit',
+                });
+            }
+        }
     }
 }
 exports.onComplete = onComplete;
--- src/ci-stats/on_complete.ts
+++ src/ci-stats/on_complete.ts
@@ -1,3 +1,4 @@
+import { execSync } from 'child_process';
 import { BuildkiteClient } from '../buildkite';
 import { CiStatsClient } from './client';
 
@@ -10,5 +11,16 @@ export async function onComplete() {
     const status = result.success ? 'SUCCESS' : 'FAILURE';
     console.log('Job Status:', result);
     await ciStats.completeBuild(status);
+
+    if (process.env.GITHUB_PR_NUMBER) {
+      const report = await ciStats.getPrReport(process.env.CI_STATS_BUILD_ID);
+      if (report?.md) {
+        process.env.CI_STATS_REPORT = report.md;
+
+        execSync('buildkite-agent meta-data set pr_comment:ci_stats_report:body "$CI_STATS_REPORT"', {
+          stdio: 'inherit',
+        });
+      }
+    }
   }
 }
